05bb7241b6d4d7c46f9b8f70471983810f45dfac
adfmb <actuario.alfredo.mb@gmail.com>
2018-05-17 18:29:26 -0500

    fileEncoding = cp932

--- test/change_vars01.R
+++ test/change_vars01.R
@@ -1,2 +1,2 @@
-bd1<-read.csv("data/USIDNET.csv",header = T)
+bd1<-read.csv("data/USIDNET.csv",header = T,sep=",",fileEncoding = "cp932")
 dic_nvasvars<-read.csv("data/Campos_USIDNET.csv",header = T)
