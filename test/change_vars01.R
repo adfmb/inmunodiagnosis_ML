f3f51c46bd8b5d92d81b2f90aaa939e89803865f
adfmb <actuario.alfredo.mb@gmail.com>
2018-05-17 19:18:30 -0500

    pruebas con nombres con comas y espacios

--- test/change_vars01.R
+++ test/change_vars01.R
@@ -1,2 +1,14 @@
-bd1<-read.csv("data/USIDNET.csv",header = T,sep=",",fileEncoding = "cp932")
+USIDNET<-read.csv("data/USIDNET.csv",header=F,sep=",",fileEncoding = "cp932")
+names(bd1)
+View(head(USIDNET))
+saveRDS(USIDNET,"data/USIDNET.rds")
+
 dic_nvasvars<-read.csv("data/Campos_USIDNET.csv",header = T)
+dic_nvasvars$Campo.Antiguo
+names(bd1)[grep("Infection_Rash",names(bd1))]
+dic_nvasvars$Campo.Antiguo[grep("Infection_Rash",dic_nvasvars$Campo.Antiguo)]
+
+names(bd1)[grep("Infection_Pneumonitis",names(bd1))]
+dic_nvasvars$Campo.Antiguo[grep("Infection_Pneumonitis",dic_nvasvars$Campo.Antiguo)]
+
+## checar regular_expressions para las comas, los puntos, los espacios, diagonales, signos de porcentajes,etc etc etc
